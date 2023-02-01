98e0e1e0a9c55930ed81e6df9238e0c536109c7f
Eric Lundby <Eric.Lundby@gmail.com>
2023-02-01 16:27:19 -0700

    attempted fix for windows test

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -10,7 +10,7 @@ if errorlevel 1 exit 1
 
 if not exist hello.grpc.pb.h exit 1
 if not exist hello.grpc.pb.cc exit 1
-if not exist %PREFIX%\\Library\\lib\\address_sorting{{ libext }} exit 1
-if not exist %PREFIX%\\Library\\lib\\gpr{{ libext }} exit 1
-if not exist %PREFIX%\\Library\\lib\\grpc{{ libext }} exit 1
-if not exist %PREFIX%\\Library\\lib\\grpc++{{ libext }} exit 1
+if not exist %PREFIX%\\Library\\lib\\address_sorting%LIB_EXT% exit 1
+if not exist %PREFIX%\\Library\\lib\\gpr%LIB_EXT% exit 1
+if not exist %PREFIX%\\Library\\lib\\grpc%LIB_EXT% exit 1
+if not exist %PREFIX%\\Library\\lib\\grpc++%LIB_EXT% exit 1
