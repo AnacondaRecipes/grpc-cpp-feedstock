31f300dcbd04c8897381bbc5b79c5a5691992c35
Marco Esters <mesters@anaconda.com>
2023-04-24 14:32:45 -0700

    Add -D_LIBCPP_DISABLE_AVAILABILITY for osx-64

--- recipe/build-cpp.sh
+++ recipe/build-cpp.sh
@@ -59,6 +59,11 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
   )
 fi
 
+if [[ "${target_platform}" == osx-* ]]; then
+  # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+  CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 mkdir -p build-cpp
 pushd build-cpp
 
