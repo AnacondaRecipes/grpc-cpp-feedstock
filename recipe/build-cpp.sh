5e5e3b037dcdc09982e6bed6ce6ff6a600167e34
Marco Esters <mesters@anaconda.com>
2023-04-24 18:00:05 -0700

    Remove cross-compilation

--- recipe/build-cpp.sh
+++ recipe/build-cpp.sh
@@ -13,52 +13,6 @@ else
   export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_CXX_STANDARD=17"
 fi
 
-if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
-  (
-    mkdir -p build-host
-    pushd build-host
-
-    export CC=$CC_FOR_BUILD
-    export CXX=$CXX_FOR_BUILD
-    export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
-    export PKG_CONFIG_PATH=${PKG_CONFIG_PATH//$PREFIX/$BUILD_PREFIX}
-    export CMAKE_ARGS=${CMAKE_ARGS//$PREFIX/$BUILD_PREFIX}
-
-    # Unset them as we're ok with builds that are either slow or non-portable
-    unset CFLAGS
-    unset CXXFLAGS
-
-    cmake ${CMAKE_ARGS} ..  \
-          -GNinja \
-          -DBUILD_SHARED_LIBS=OFF \
-          -DCMAKE_BUILD_TYPE=Release \
-          -DCMAKE_PREFIX_PATH=$BUILD_PREFIX \
-          -DCMAKE_INSTALL_PREFIX=$BUILD_PREFIX \
-          -DgRPC_CARES_PROVIDER="package" \
-          -DgRPC_GFLAGS_PROVIDER="package" \
-          -DgRPC_PROTOBUF_PROVIDER="package" \
-          -DProtobuf_ROOT=$BUILD_PREFIX \
-          -DCMAKE_POLICY_DEFAULT_CMP0074=NEW \
-          -DgRPC_SSL_PROVIDER="package" \
-          -DgRPC_ZLIB_PROVIDER="package" \
-          -DgRPC_ABSL_PROVIDER="package" \
-          -DgRPC_RE2_PROVIDER="package" \
-          -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc \
-          -DgRPC_BUILD_CODEGEN=ON \
-          -DgRPC_BUILD_CSHARP_EXT=OFF \
-          -DgRPC_BUILD_GRPC_CSHARP_PLUGIN=OFF \
-          -DgRPC_BUILD_GRPC_NODE_PLUGIN=OFF \
-          -DgRPC_BUILD_GRPC_OBJECTIVE_C_PLUGIN=OFF \
-          -DgRPC_BUILD_GRPC_PHP_PLUGIN=OFF \
-          -DgRPC_BUILD_GRPC_PYTHON_PLUGIN=OFF \
-          -DgRPC_BUILD_GRPC_RUBY_PLUGIN=OFF
-    ninja grpc_cpp_plugin
-    cp grpc_cpp_plugin $BUILD_PREFIX/bin/grpc_cpp_plugin
-
-    popd
-  )
-fi
-
 if [[ "${target_platform}" == osx-* ]]; then
   # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
   CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
