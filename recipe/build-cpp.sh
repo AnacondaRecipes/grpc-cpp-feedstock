97a14befe13f7cfacd156e33dd05ec6c4ad1afa8
Serhii Kupriienko <skupriienko@anaconda.com>
2023-10-23 10:21:05 +0300

    Remove duplication

--- recipe/build-cpp.sh
+++ recipe/build-cpp.sh
@@ -16,11 +16,6 @@ else
     export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_CXX_STANDARD=17"
 fi
 
-if [[ "${target_platform}" == osx-* ]]; then
-    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
-    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-fi
-
 mkdir -p build-cpp
 pushd build-cpp
 
