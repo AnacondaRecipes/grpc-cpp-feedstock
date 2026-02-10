5ad385bdb09ca4a686469464b0425261b1db71ff
vkhomits <vkhomits@anaconda.com>
2026-02-10 17:38:52 +0200

    Update version to 1.78.0 (#39)
    * Update version to 1.78.0
    
    
    Co-authored-by: skupr-anaconda <79282962+skupr-anaconda@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: vkhomits <khomitsvalia@gmail.com>
    Co-authored-by: skupr-anaconda <79282962+skupr-anaconda@users.noreply.github.com>

--- recipe/build-grpcio.sh
+++ recipe/build-grpcio.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export GRPC_BUILD_WITH_BORING_SSL_ASM=""
+export GRPC_PYTHON_BUILD_SYSTEM_ABSL="True"
 export GRPC_PYTHON_BUILD_SYSTEM_CARES="True"
 export GRPC_PYTHON_BUILD_SYSTEM_GRPC_CORE="True"
 export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL="True"
@@ -8,12 +9,12 @@ export GRPC_PYTHON_BUILD_SYSTEM_RE2="True"
 export GRPC_PYTHON_BUILD_SYSTEM_ZLIB="True"
 export GRPC_PYTHON_BUILD_WITH_CYTHON="True"
 export GRPC_PYTHON_USE_PREBUILT_GRPC_CORE=""
-export GRPC_RE2_LIBRARY="${PREFIX}/lib/libre2.${SHLIB_EXT}"
 
 if [[ "${target_platform}" == linux-* ]]; then
     # set these so the default in setup.py are not used
     # it seems that we need to link to pthread on linux or osx.
     export GRPC_PYTHON_LDFLAGS="-lpthread"
+    export CXXFLAGS="$CXXFLAGS -fpermissive"
 elif [[ "$target_platform" == osx-* ]]; then
     export GRPC_PYTHON_LDFLAGS=" -framework CoreFoundation"
 fi
@@ -24,5 +25,7 @@ fi
 
 ln -sf "$(which $CC)" "$SRC_DIR/cc"
 export PATH="$SRC_DIR:$PATH"
+export DYLD_LIBRARY_PATH="${PREFIX}/lib:${DYLD_LIBRARY_PATH}"
+CXXFLAGS="${CXXFLAGS} -DNDEBUG"
 
 $PYTHON -m pip install . --no-deps --no-build-isolation --ignore-installed --no-cache-dir -v
